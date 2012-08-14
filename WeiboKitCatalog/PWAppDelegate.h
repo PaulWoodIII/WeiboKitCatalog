4450bc3d766d2d1ef625c65c291b7acef5a68677
Paul Wood III <paul.wood.iii@gmail.com>
2012-08-14 11:04:40 +0800

    Initial Commit

---
+++ WeiboKitCatalog/PWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PWAppDelegate.h
+//  WeiboKitCatalog
+//
+//  Created by Paul Wood on 8/14/12.
+//  Copyright (c) 2012 Paul Wood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
